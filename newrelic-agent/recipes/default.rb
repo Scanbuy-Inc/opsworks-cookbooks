975047f2b8a97e994a6c03e98aa9bd4a567baa21
kd2anu <shawn.ko@scanbuy.com>
2017-02-03 17:02:04 +0000

    change newrelic-agent install logic

--- newrelic-agent/recipes/default.rb
+++ newrelic-agent/recipes/default.rb
@@ -4,15 +4,30 @@ tomcat_group=node['tomcat']['tomcat_group']
 application = node[:opsworks][:instance][:hostname].chop
 country = node[:opsworks][:stack][:name][-2,2]
 
+service 'tomcat7' do
+  action :nothing
+  only_if {File.exists?("/etc/init.d/tomcat7")}
+  only_if %Q{/usr/bin/yum list installed tomcat-native 2>&1 >> /dev/null && true || false}
+end
+
+execute 'install_newrelic_agent' do
+  action :nothing
+  command "/usr/bin/java -jar newrelic.jar install"
+  cwd "#{install_dir}/newrelic/"
+  only_if { File.directory?("#{install_dir}/newrelic") }
+  notifies :restart, 'service[tomcat7]', :immediately
+end
+
 cookbook_file "#{install_dir}/newrelic-java-3.31.0.zip" do
   source "newrelic-java-3.31.0.zip"
   mode 0644
+  not_if { File.exists?("#{install_dir}/newrelic-java-3.31.0.zip") }
 end
 
 execute 'unzip_package' do
   command "/usr/bin/unzip #{install_dir}/newrelic-java-3.31.0.zip -d #{install_dir}"
   cwd '/root'
-  only_if { File.exists?("#{install_dir}/newrelic-java-3.31.0.zip") }
+  not_if { File.directory?("#{install_dir}/newrelic/") }
 end
 
 template "#{install_dir}/newrelic/newrelic.yml" do
@@ -23,17 +38,5 @@ template "#{install_dir}/newrelic/newrelic.yml" do
   variables({
      :appname => "#{application}_#{country}"
   })
-  only_if { File.exists?("/usr/share/tomcat7/newrelic/newrelic.yml") }
-end
-
-execute 'install_newrelic_agent' do
-  command "/usr/bin/java -jar newrelic.jar install"
-  cwd "#{install_dir}/newrelic/"
-  only_if { File.directory?("/usr/share/tomcat7/newrelic") }
-end
-
-service 'tomcat7' do
-  action :restart
-  only_if {File.exists?("/etc/init.d/tomcat7")}
-  only_if %Q{/usr/bin/yum list installed tomcat-native 2>&1 >> /dev/null && true || false}
+  notifies :run, 'execute[install_newrelic_agent]', :immediately
 end
