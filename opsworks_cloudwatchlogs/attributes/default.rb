d5d0185d0e57462376a76b115cc5ce9426ae3c5d
kd2anu <kd2anu.7@gmail.com>
2017-04-13 12:19:27 -0400

    roll back

--- opsworks_cloudwatchlogs/attributes/default.rb
+++ opsworks_cloudwatchlogs/attributes/default.rb
@@ -10,4 +10,3 @@ end
 
 default["cloudwatchlogs"]["AGENT_LOGS"] = "/var/log/aws/opsworks/*.log"
 default["cloudwatchlogs"]["CHEF_LOGS"] = "/var/lib/aws/opsworks/chef/*.log"
-default["opsworks"]["cloud_watch_logs_configurations"] = nil
